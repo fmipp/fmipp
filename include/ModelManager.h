074f7ae8ebd8a6b1c2b52cb789db9686a6eb09b6
Atiyah Elsheikh <a.m.g.elsheikh@gmail.com>
2013-07-17 16:31:32 +0200

    ModelManager is documented as a first test , feedback required

--- include/ModelManager.h
+++ include/ModelManager.h
@@ -3,6 +3,24 @@
  * All rights reserved. See file FMIPP_LICENSE for details.
  * --------------------------------------------------------------*/
 
+
+/**
+ * /file ModelManager.h 
+ * /class ModelManager.h provides the basic routines with which FMU models are managed 
+ *                       acc. to a singlton pattern.  
+ * 
+ * An instance of ModelManager preserves basic info about the FMU, its path, model name, provided 
+ * dll library and the XML description file. 
+ * It provides a portable implementation regardless of the platform that has been used for generating 
+ * the employed FMUs. An instance of model manager:
+ * 1. is privately constructed and cannot be externally instantiated 
+ * 2. However, an address of the instance can be accessed 
+ * 3. from the fmuPath/modelName or xmlPath/dllPath/modelName,
+ *    (in both cases the fmu has to be already unzipped and the unzipped materials should follow 
+ *    the standard naming conventions?) 
+ * 
+ */ 
+
 #ifndef _FMIPP_MODELMANAGER_H
 #define _FMIPP_MODELMANAGER_H
 
@@ -17,39 +35,23 @@ class ModelManager {
 
 public:
 
-	~ModelManager();
-
-	/** Get singleton instance of model manager. **/
-	static ModelManager& getModelManager();
-
-	/** Get model (from standard unzipped FMU). **/
+        
+	~ModelManager();  ///< Descrtructor.
+	static ModelManager& getModelManager();  ///< Get singleton instance of model manager. 
 	static FMU_functions* getModel( const std::string& fmuPath,
-					const std::string& modelName );
-
-	/** Get model (from non-standard 'modelName.xml' and 'modelName.dll'). **/
+					const std::string& modelName ); ///< Get model (from standard unzipped FMU). 
 	static FMU_functions* getModel( const std::string& xmlPath,
 					const std::string& dllPath,
-					const std::string& modelName );
+					const std::string& modelName ); ///< Get model (from non-standard 'modelName.xml' and 'modelName.dll').  
 
 private:
 
-	/** Private constructor (singleton). **/
-	ModelManager() {}
-
-	/** Helper function for loading FMU shared library **/
-	static int loadDll( std::string dllPath, FMU_functions* fmuFun );
-
-	/** Helper function for loading FMU shared library **/
-	static void* getAdr( int* s, FMU_functions *fmuFun, const char* functionName );
-
-	/** Pointer to singleton instance. **/
-	static ModelManager* modelManager_;
-
-	/** Define container for description collection. **/
-	typedef std::map<std::string, FMU_functions*> Descriptions;
-
-	/** Collection of descriptions. **/
-	Descriptions modelDescriptions_;
+	ModelManager() {}  ///< Private constructor (singleton). 
+	static int loadDll( std::string dllPath, FMU_functions* fmuFun );               ///< Helper function for loading FMU shared library. 
+	static void* getAdr( int* s, FMU_functions *fmuFun, const char* functionName ); ///< Helper function for loading FMU shared library 
+	static ModelManager* modelManager_;                         ///< Pointer to singleton instance. 
+	typedef std::map<std::string, FMU_functions*> Descriptions; ///< Define container for description collection. 
+	Descriptions modelDescriptions_;                            ///< Collection of descriptions of FMUs.
 };
 
 
