4975696b68d7706edd11d090b92bec013c1e1e0e
Edmund Widl <edmund.widl@ait.ac.at>
2014-03-14 18:43:55 +0100

    added target for creating documentation with Doxygen, added more tests (almost dummy, need implementation)

---
+++ test/testFixedStepSizeFMU.cpp
@@ -0,0 +1,43 @@
+#include <FixedStepSizeFMU.h>
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE testFMU
+#include <boost/test/unit_test.hpp>
+#include <cmath>
+
+
+BOOST_AUTO_TEST_CASE( test_fmu_load )
+{
+	std::string MODELNAME( "sine_standalone" );
+	FixedStepSizeFMU fmu( FMU_URI_PRE + MODELNAME, MODELNAME );
+}
+
+BOOST_AUTO_TEST_CASE( test_fmu_instantiate )
+{
+	// FIXME: Implement test here.
+	BOOST_REQUIRE( false );
+}
+
+BOOST_AUTO_TEST_CASE( test_fmu_initialize )
+{
+	// FIXME: Implement test here.
+	BOOST_REQUIRE( false );
+}
+
+BOOST_AUTO_TEST_CASE( test_fmu_getvalue )
+{
+	// FIXME: Implement test here.
+	BOOST_REQUIRE( false );
+}
+
+BOOST_AUTO_TEST_CASE( test_fmu_setvalue )
+{
+	// FIXME: Implement test here.
+	BOOST_REQUIRE( false );
+}
+
+BOOST_AUTO_TEST_CASE( test_fmu_run_simulation )
+{
+	// FIXME: Implement test here.
+	BOOST_REQUIRE( false );
+}
