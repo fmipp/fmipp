0b9e68d2eb5e06818f963fdd0dab9254449f1cb5
Edmund Widl <edmund.widl@ait.ac.at>
2014-05-08 14:36:07 +0200

    added tests for (Interpolating)FixedStepSizeFMU

--- test/testFixedStepSizeFMU.cpp
+++ test/testFixedStepSizeFMU.cpp
@@ -3,41 +3,94 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE testFMU
 #include <boost/test/unit_test.hpp>
+
+#define _USE_MATH_DEFINES
 #include <cmath>
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
 
 
 BOOST_AUTO_TEST_CASE( test_fmu_load )
 {
-	std::string MODELNAME( "sine_standalone" );
-	FixedStepSizeFMU fmu( FMU_URI_PRE + MODELNAME, MODELNAME );
+	std::string modelName( "sine_standalone" );
+	FixedStepSizeFMU fmu( std::string( FMU_URI_PRE ) + modelName, modelName );
 }
 
-BOOST_AUTO_TEST_CASE( test_fmu_instantiate )
+
+BOOST_AUTO_TEST_CASE( test_fmu_init )
 {
-	// FIXME: Implement test here.
-	BOOST_REQUIRE( false );
+	std::string modelName( "sine_standalone" );
+	FixedStepSizeFMU fmu( std::string( FMU_URI_PRE ) + modelName, modelName );
+
+	std::string initRealInputNames[1] = { "omega" };
+	double initRealInputVals[1] = { 0.1 * M_PI };
+
+	const double startTime = 0.0;
+	const double stepSize = 1.0; // NB: fixed step size enforced by FMU!
+
+	int status = fmu.init( "test_sine", initRealInputNames, initRealInputVals, 1, startTime, stepSize );
+	BOOST_REQUIRE_MESSAGE( 1 == status, "init(...) FAILED" );
 }
 
-BOOST_AUTO_TEST_CASE( test_fmu_initialize )
+
+BOOST_AUTO_TEST_CASE( test_fmu_getrealoutputs )
 {
-	// FIXME: Implement test here.
-	BOOST_REQUIRE( false );
+	std::string modelName( "sine_standalone" );
+	FixedStepSizeFMU fmu( std::string( FMU_URI_PRE ) + modelName, modelName );
+
+	std::string initRealInputNames[1] = { "omega" };
+	double initRealInputVals[1] = { 0.1 * M_PI };
+
+	const double startTime = 0.0;
+	const double stepSize = 1.0; // NB: fixed step size enforced by FMU!
+
+	std::string realOutputNames[1] = { "phi" };
+
+	fmu.defineRealOutputs( realOutputNames, 1 );
+
+	int status = fmu.init( "test_sine", initRealInputNames, initRealInputVals, 1, startTime, stepSize );
+	BOOST_REQUIRE_MESSAGE( 1 == status, "init(...) FAILED" );
+
+	double* result = fmu.getRealOutputs();
+	BOOST_REQUIRE( result[0] == 0.0 );
 }
 
-BOOST_AUTO_TEST_CASE( test_fmu_getvalue )
-{
-	// FIXME: Implement test here.
-	BOOST_REQUIRE( false );
-}
-
-BOOST_AUTO_TEST_CASE( test_fmu_setvalue )
-{
-	// FIXME: Implement test here.
-	BOOST_REQUIRE( false );
-}
 
 BOOST_AUTO_TEST_CASE( test_fmu_run_simulation )
 {
-	// FIXME: Implement test here.
-	BOOST_REQUIRE( false );
+	std::string modelName( "sine_standalone" );
+	FixedStepSizeFMU fmu( std::string( FMU_URI_PRE ) + modelName, modelName );
+
+	std::string initRealInputNames[1] = { "omega" };
+	double initRealInputVals[1] = { 0.1 * M_PI };
+
+	const double startTime = 0.0;
+	const double stepSize = 1.0; // NB: fixed step size enforced by FMU!
+
+	std::string realOutputNames[1] = { "phi" };
+
+	fmu.defineRealOutputs( realOutputNames, 1 );
+
+	int status = fmu.init( "test_sine", initRealInputNames, initRealInputVals, 1, startTime, stepSize );
+	BOOST_REQUIRE_MESSAGE( 1 == status, "init(...) FAILED" );
+
+	const double stopTime = 5.0;
+	const double deltaTime = 0.2;
+	double time = startTime;
+	double* result;
+	double reference;
+	while ( time <= stopTime )
+	{
+		fmu.sync( time, time + deltaTime );
+		time += deltaTime;
+
+		result = fmu.getRealOutputs();
+		reference = std::sin( 0.1 * M_PI * stepSize * std::floor( time/stepSize ) );
+
+		BOOST_REQUIRE_MESSAGE( std::fabs( result[0] - reference ) < 1e-8,
+				       "result mismatch: deltaResult = " << ( result[0] - reference ) );
+	}
 }
--- test/testInterpolatingFixedStepSizeFMU.cpp
+++ test/testInterpolatingFixedStepSizeFMU.cpp
@@ -3,41 +3,96 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE testFMU
 #include <boost/test/unit_test.hpp>
+
+#define _USE_MATH_DEFINES
 #include <cmath>
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 
 
 BOOST_AUTO_TEST_CASE( test_fmu_load )
 {
-	std::string MODELNAME( "sine_standalone" );
-	InterpolatingFixedStepSizeFMU fmu( FMU_URI_PRE + MODELNAME, MODELNAME );
+	std::string modelName( "sine_standalone" );
+	InterpolatingFixedStepSizeFMU fmu( std::string( FMU_URI_PRE ) + modelName, modelName );
 }
 
-BOOST_AUTO_TEST_CASE( test_fmu_instantiate )
+
+BOOST_AUTO_TEST_CASE( test_fmu_init )
 {
-	// FIXME: Implement test here.
-	BOOST_REQUIRE( false );
+	std::string modelName( "sine_standalone" );
+	InterpolatingFixedStepSizeFMU fmu( std::string( FMU_URI_PRE ) + modelName, modelName );
+
+	std::string initRealInputNames[1] = { "omega" };
+	double initRealInputVals[1] = { 0.1 * M_PI };
+
+	const double startTime = 0.0;
+	const double stepSize = 1.0; // NB: fixed step size enforced by FMU!
+
+	int status = fmu.init( "test_sine", initRealInputNames, initRealInputVals, 1, startTime, stepSize );
+	BOOST_REQUIRE_MESSAGE( 1 == status, "init(...) FAILED" );
 }
 
-BOOST_AUTO_TEST_CASE( test_fmu_initialize )
+
+BOOST_AUTO_TEST_CASE( test_fmu_getrealoutputs )
 {
-	// FIXME: Implement test here.
-	BOOST_REQUIRE( false );
+	std::string modelName( "sine_standalone" );
+	InterpolatingFixedStepSizeFMU fmu( std::string( FMU_URI_PRE ) + modelName, modelName );
+
+	std::string initRealInputNames[1] = { "omega" };
+	double initRealInputVals[1] = { 0.1 * M_PI };
+
+	const double startTime = 0.0;
+	const double stepSize = 1.0; // NB: fixed step size enforced by FMU!
+
+	std::string realOutputNames[1] = { "phi" };
+
+	fmu.defineRealOutputs( realOutputNames, 1 );
+
+	int status = fmu.init( "test_sine", initRealInputNames, initRealInputVals, 1, startTime, stepSize );
+	BOOST_REQUIRE_MESSAGE( 1 == status, "init(...) FAILED" );
+
+	double* result = fmu.getRealOutputs();
+	BOOST_REQUIRE( result[0] == 0.0 );
 }
 
-BOOST_AUTO_TEST_CASE( test_fmu_getvalue )
-{
-	// FIXME: Implement test here.
-	BOOST_REQUIRE( false );
-}
-
-BOOST_AUTO_TEST_CASE( test_fmu_setvalue )
-{
-	// FIXME: Implement test here.
-	BOOST_REQUIRE( false );
-}
 
 BOOST_AUTO_TEST_CASE( test_fmu_run_simulation )
 {
-	// FIXME: Implement test here.
-	BOOST_REQUIRE( false );
+	std::string modelName( "sine_standalone" );
+	InterpolatingFixedStepSizeFMU fmu( std::string( FMU_URI_PRE ) + modelName, modelName );
+
+	std::string initRealInputNames[1] = { "omega" };
+	double initRealInputVals[1] = { 0.1 * M_PI };
+
+	const double startTime = 0.0;
+	const double stepSize = 1.0; // NB: fixed step size enforced by FMU!
+
+	std::string realOutputNames[1] = { "phi" };
+
+	fmu.defineRealOutputs( realOutputNames, 1 );
+
+	int status = fmu.init( "test_sine", initRealInputNames, initRealInputVals, 1, startTime, stepSize );
+	BOOST_REQUIRE_MESSAGE( 1 == status, "init(...) FAILED" );
+
+	const double stopTime = 5.0;
+	const double deltaTime = 0.2;
+	double time = startTime;
+	while ( time <= stopTime )
+	{
+		fmu.sync( time, time + deltaTime );
+		time += deltaTime;
+
+		double* result = fmu.getRealOutputs();
+
+		double t0 = stepSize * std::floor( time/stepSize );
+		double t1 = t0 + stepSize;
+		double x0 = std::sin( 0.1 * M_PI * t0 );
+		double x1 = std::sin( 0.1 * M_PI * t1 );
+		double reference = x0 + ( time - t0 )*( x1 - x0 )/( t1 - t0 );
+
+		BOOST_REQUIRE_MESSAGE( std::fabs( result[0] - reference ) < 1e-8,
+				       "result mismatch: deltaResult = " << ( result[0] - reference ) );
+	}
 }
