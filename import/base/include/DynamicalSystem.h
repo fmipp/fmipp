71d470be050d358a0c086b32713be42f913e1b00
Gregor de Cillia <gregor.de-cillia.fl@ait.ac.at>
2015-04-30 15:41:47 +0200

    DynamicalSystem Class
    First commit for importing the 2.0 brach:
    
    The class DynamicalSystem will later be used to determine which parts of FMUModelExchangeBase are exposed to the integrator class and
    which are not. The main idea is "keep eventhandling ut of the integration" meaning that the job of an integrator is to locate events,
    not handle them.

---
+++ import/base/include/DynamicalSystem.h
@@ -0,0 +1,81 @@
+#ifndef _DYNAMICAL_SYSTEM_H
+#define _DYNAMICAL_SYSTEM_H
+
+typedef double real_type;
+typedef real_type time_type;
+
+//#include "import/integrators/include/IntegratorProperties.h"
+#include "common/fmi_v1.0/fmiModelTypes.h"
+#include "import/integrators/include/Integrator.h"
+
+
+/**
+ * \file DynamicalSystem.h
+ *
+ * \class DynamicalSystem DynamicalSystem.h
+ * Reduces FMUs for ME to the functionalities necessary for integration. It also defines which parts
+ * of an fmu are actually exposed to the Integrator and IntegratorStepper class
+ *
+ * these functionalities are
+ *   * get/set States
+ *   * get/set Time
+ *   * get     EventIndicators
+ *   * get     Derivatives (  righthandside of the ODE )
+ *   * get     Jacobian
+ *
+ * In case an FMU does not supply a Jacobian, the jacobian function is calculated using a finite
+ * difference method.
+ *
+ * MAINTAINANCE NOTE: make sure to keep this class slim, since the evaluation speed of this functions
+ *                    has a tremendous effect on the overall performance of the fmippim module
+ */
+
+struct DynamicalSystem{
+	/// set the FMU time
+	virtual void setTime( time_type time ) = 0;
+	/// get the FMU time
+	virtual real_type getTime() const = 0;  //\TODO: change the input of Integrator from
+	//uncomment
+
+	/// get continuous states
+	virtual fmiStatus getContinuousStates( real_type* x ) = 0;
+
+	/// set continuous states
+	virtual fmiStatus setContinuousStates( const real_type* x ) = 0;
+
+	/// get the derivatives / righthandside of the ODE
+	virtual fmiStatus getDerivatives( real_type* dx ) = 0;
+
+	/// get EventIndicators at curret FMU state/time
+	virtual fmiStatus getEventIndicators( real_type* eventsind ) = 0;
+
+	/// return the number of continuous states
+	virtual std::size_t nStates() const = 0;
+
+	/// return the number of event indicators
+	virtual std::size_t nEventInds() const = 0;
+
+	/// say wheteher the FMU provides a jacobian ( always false for 1.0 FMUs )
+	virtual bool providesJacobian(){ return false; };
+
+	/// get Jacobian for the current FMU state/time
+	//virtual void getJac( real_type** J );
+
+
+	/** calculate the Jacobian and store the result as c-array (double*) of length NEQ*NEQ
+	 *
+	 * For example, J = ( J00 J01 )   gets returned as ( J00, J01, J10, J11 )
+	 *                  ( J10 J11 )
+	 *
+	 * the method used is of 8th order and uses 8*NEQ rhs evaluations.
+	 *
+	 * for comparison - the simplest method (1st order) uses NEQ+1 rhs evaluations.
+	 */
+	virtual void getNumericalJacobian( real_type* J, const real_type* x, real_type* dfdt, const real_type t );
+
+
+	/// check wether the current event indicators differ from the input
+	bool checkStateEvent( real_type* eventsind );
+};
+
+#endif
---
+++ import/base/src/DynamicalSystem.cpp
@@ -0,0 +1,89 @@
+#include "import/base/include/DynamicalSystem.h"
+#include "import/base/include/NumericalJacobianCoefficients.icc"
+
+void DynamicalSystem::getNumericalJacobian( real_type* J, const real_type* x, real_type* dfdt, const real_type t )
+{
+	const int steps = 3;            // determines the order of accuracy for the Jacobian
+	                                // and also influences the runtime
+        NumericalJacobianCoefficients<steps> coefs;
+	double* xp = (double*) x;       // using a copy would be safer
+	double* Jp = J;
+	const int N = nStates();
+	double* dx = new double( N );   // \TODO: make dx a class member to prevent permenent
+	                                //        allocation/deallocation of memory
+	setTime( t );
+	setContinuousStates( xp );
+
+	// step size for the finite difference
+	double delta = 1.0e-5;   // \TODO: this value for delta is suited for a certain system.
+	                         //        find a solution that is not hardware specific.
+	double h = delta;
+
+	for( int j = 0; j < N; j++ ){
+		/// calculate the j-th column of the jacobian matrix
+
+		/// clear the matrix column
+		for( int i = 0; i < N; i++ ){
+			Jp[N*i+j] = 0;
+		}
+
+		/// use a k step metod
+		for( int k = 0; k < steps; k++ ){
+			// add the vector
+			//     coefs[k] * ( f(x+(k+1)ej*h)-f(x-(k+1)*ej)*h )/h
+			// to the j-th column of J. where
+			//     e1 = (1,0,0,0,...,0)
+			//     e2 = (0,1,0,0,...,0)
+			//     e3 = (0,0,1,0,....0)
+			//     ....
+			//     eN = (0,0,...,0,0,1)
+
+			xp[j] += ( k + 1.0 )*h;
+			setContinuousStates( xp );
+			getDerivatives( dx );
+			for( int i = 0; i < N; i++ )
+				{
+					Jp[N*i+j] += dx[i]*coefs[k]/h;
+				}
+			xp[j] -= 2.0*( k + 1.0 )*h;
+			setContinuousStates( xp );
+			getDerivatives( dx );
+			for( int i = 0; i < N; i++ )
+				{
+					Jp[N*i+j] -= dx[i]*coefs[k]/h;
+				}
+			xp[j] += ( k + 1.0 )*h;
+		}
+	}
+	setContinuousStates( xp );
+	double t2 = t;
+	for( int i = 0; i < N; i++ )
+		dfdt[i] = 0.0;
+	for( int k = 0; k < steps; k++ ){
+		t2 += (k+1.0)*h;
+		setTime( t2 );
+		getDerivatives( dx );
+		for( int i = 0; i < N; i++ )
+			{
+				dfdt[i] += dx[i]*coefs[k]/h;
+			}
+		t2 -= 2.0*( k + 1.0 )*h;
+		setTime( t2 );
+		getDerivatives( dx );
+		for( int i = 0; i < N; i++ )
+			{
+				dfdt[i] -= dx[i]*coefs[k]/h;
+			}
+		t2 += (k+1.0)*h;
+	}
+	delete dx;
+}
+
+bool DynamicalSystem::checkStateEvent( real_type* eventsind ){
+	real_type preeventsind [ nEventInds() ];
+	getEventIndicators( preeventsind );
+	bool stateEvent = false;
+	for ( size_t i = 0; i < nEventInds(); i++ )
+		stateEvent = stateEvent || ( preeventsind[i] * eventsind[i] < 0 );
+	return stateEvent;
+};
