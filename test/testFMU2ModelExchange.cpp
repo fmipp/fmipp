2c5a23f4196aff26ac1a06a9897c0d4049d34920
Gregor de Cillia <Gregor.De-Cillia.fl@ait.ac.at>
2015-05-12 19:59:49 +0200

    Add test testFMU2ModelExchange
    Tests loading of FMUs of thype 2.0 as well as the Jacobian function.

---
+++ test/testFMU2ModelExchange.cpp
@@ -0,0 +1,163 @@
+#include <import/base/include/FMUModelExchange_v2.h>
+#include <import/base/include/ModelManager.h>
+
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE testFMU2ModelExchange
+
+#include <boost/test/unit_test.hpp>
+#include <boost/format.hpp>
+#include <iostream>
+
+#if defined( WIN32 ) // Windows.
+#include <algorithm>
+#define fmin min
+#else // Linux, Unix, etc.
+#include <cmath>
+#endif
+
+using namespace std;
+using namespace boost; // for std::cout << boost::format( ... ) % ... % ... ;
+using namespace fmi_2_0;
+
+// the first few test are almost exact copies of testFMUModelExchange
+BOOST_AUTO_TEST_CASE( test_fmu_load_faulty )
+{
+	string MODELNAME( "XYZ" );
+	FMUModelExchange fmu( "ABC", MODELNAME, fmi2False, EPS_TIME );
+	fmiStatus status = fmu.instantiate( "xyz", fmi2False );
+	BOOST_REQUIRE( status == fmiError );
+}
+
+string fmuPath( "numeric/" );
+
+BOOST_AUTO_TEST_CASE( test_fmu_load )
+{
+	string MODELNAME( "stiff2" );
+	FMUModelExchange fmu( FMU_URI_PRE + fmuPath + MODELNAME, MODELNAME, fmi2False, EPS_TIME );
+}
+
+BOOST_AUTO_TEST_CASE( test_fmu_instantiate )
+{
+	string MODELNAME( "stiff2" );
+	FMUModelExchange fmu( FMU_URI_PRE + fmuPath + MODELNAME, MODELNAME, fmi2False, EPS_TIME );
+	fmiStatus status = fmu.instantiate( "stiff21", fmi2False );
+	BOOST_REQUIRE( status == fmiOK );
+}
+
+BOOST_AUTO_TEST_CASE( test_fmu_initialize )
+{
+	string MODELNAME( "stiff2" );
+	FMUModelExchange fmu( FMU_URI_PRE + fmuPath + MODELNAME, MODELNAME, fmi2False, EPS_TIME );
+	fmu.instantiate( "stiff21", fmi2False );
+	fmiStatus status = fmu.initialize();
+	BOOST_REQUIRE( status == fmiOK );
+}
+
+BOOST_AUTO_TEST_CASE( test_setters_and_getters )
+{
+	cout << endl << "---- GETTERS AND SETTERS ----" << endl << endl;
+
+	string MODELNAME( "stiff2" );
+	FMUModelExchange fmu( FMU_URI_PRE + fmuPath + MODELNAME, MODELNAME, fmi2False, EPS_TIME );
+	fmu.instantiate( "stiff21", fmi2False );
+	fmu.initialize();
+
+	fmi2Real y;
+	fmiStatus status;
+
+	// test getValue
+	cout << "values after initialize():" << endl;
+	status = fmu.getValue( "x", y );
+	BOOST_REQUIRE( status == fmiOK );
+	cout << format( "%-8s %-E\n" ) % "x" % y;
+	fmu.getValue( "x0", y );
+	cout << format( "%-8s %-E\n" ) % "x0" % y;
+	fmu.getValue( "ts", y );
+	cout << format( "%-8s %-E\n" ) % "ts" % y;
+	fmu.getValue( "k", y );
+	cout << format( "%-8s %-E\n" ) % "k" % y;
+
+	// test setValue
+	cout << format( "\n%-40s %-20s \n" ) % "setting x to 2.01" % "fmu.setValue( \"x\", 2.01 )";
+	status = fmu.setValue( "x", 2.01 );
+	BOOST_REQUIRE( status == fmiOK );
+	cout << format( "%-40s %-20s \n" ) % "calling getter function" % "fmu.getValue( \"x\", y )";
+	fmu.getValue( "x", y );
+	if ( y == 2.01 ){
+		cout << format( "%-40s %-20s \n" ) % "y == 2.01?" %  "fmi2True";
+	}else{
+		cout << format( "%-40s %-20s \n" ) % "y == 2.01?" %  "fmi2True";
+	}
+	BOOST_REQUIRE( y == 2.01 );
+}
+
+BOOST_AUTO_TEST_CASE( test_fmu_model_description )
+{
+	cout << endl << "---- BASIC FUNCTIONALITIES OF MODELDESCRIPTION ----" << endl << endl;
+
+	string MODELNAME( "stiff2" );
+	FMUModelExchange fmu( FMU_URI_PRE + fmuPath + MODELNAME, MODELNAME,
+			       fmi2False, EPS_TIME , IntegratorType::rk );
+
+	cout << format( "%-40s %-10E \n" ) % "number of continuous states" % fmu.nStates();
+	cout << format( "%-40s %-10E \n" ) % "number of event indicators"  % fmu.nEventInds();
+	cout << format( "%-40s %-10E \n" ) % "number of value references"  % fmu.nValueRefs();
+	if ( fmu.providesJacobian() )
+		cout << format( "%-40s %-10s \n" ) % "provides Jacobian"   % "true";
+	else
+		cout << format( "%-40s %-10s \n" ) % "provides Jacobian"   % "false";
+}
+
+BOOST_AUTO_TEST_CASE( test_rhs_jacobean_etc )
+{
+	cout << endl << "---- RHS AND JACOBIAN ----" << endl << endl;
+
+	string MODELNAME( "stiff2" );
+	FMUModelExchange fmu( FMU_URI_PRE + fmuPath + MODELNAME, MODELNAME, fmi2False, EPS_TIME );
+	fmu.instantiate( "stiff21", fmi2False );
+	fmu.initialize();
+
+	fmiStatus status;
+	fmi2Real y;
+
+	// test the RHS evaluation
+	status = fmu.getDerivatives( &y );
+	BOOST_REQUIRE( status == fmiOK );
+	cout << format( "%-40s %-E\n" ) % "derivative from statrting position" % y;
+
+	fmu.setValue( "x", 0.5 );
+	status = fmu.getDerivatives( &y );
+	BOOST_REQUIRE( status == fmiOK );
+	cout << format( "%-40s %-E\n" ) % "derivative for x = 0.5" % y;
+	BOOST_REQUIRE( y == 25 );
+
+	// initialize the Jacobian
+	fmi2Real** J = new fmi2Real*[1];
+	J[0] = new fmi2Real[1];
+
+	// test getter for jacobian
+	fmu.getJacobian( J );
+	cout << format( "%-40s %-E\n" ) % "jacobian for x = 0.5" % J[0][0];
+
+	// change x and reset Jacobian
+	status = fmu.setValue( "x", 0.1 );
+	BOOST_REQUIRE( status == fmiOK );
+	fmu.getJacobian( J );
+	cout << format( "%-40s %-E\n" ) % "jacobian for x = 0.1" % J[0][0];
+}
+
+BOOST_AUTO_TEST_CASE( test_model_manager_me )
+{
+	// almost the same as the ME test in ($build)/test/testModelManager
+	string modelName( "stiff2" );
+	string fmuUrl = string( FMU_URI_PRE ) + fmuPath + modelName;
+
+	ModelManager& manager = ModelManager::getModelManager();
+
+	BareFMU2* bareFMU1 = manager.getInstance( fmuUrl, modelName );
+	BareFMU2* bareFMU2 = manager.getInstance( fmuUrl, modelName );
+
+	BOOST_REQUIRE_MESSAGE( bareFMU1 == bareFMU2,
+			       "Bare FMUs are not equal." );
+}
