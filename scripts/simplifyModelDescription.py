01bb9130e1673415f1cfdb7e13ae7bdb0b674867
Edmund Widl <Edmund.Widl@ait.ac.at>
2015-07-08 12:50:31 +0200

    added new folder 'scripts', added python script for simplifying an FMU's model description (removes all scalar variables that are neither inputs nor outputs)

---
+++ scripts/simplifyModelDescription.py
@@ -0,0 +1,120 @@
+
+def simplifyModelDescription( xmlModelDescription ):
+        import xml.etree.ElementTree as et
+
+	# parse model description and retrieve root element
+	root = et.fromstring( xmlModelDescription )
+	
+	# get XML node listing the model variables
+	modelVariables = root.find( "ModelVariables" )
+	
+	# get list of all XML nodes representing scalar variables
+	scalars = modelVariables.findall( "./ScalarVariable" )
+	
+	# set of declared types of scalar variables that are not removed
+	declaredTypes = set()
+	
+	# iterate through all XML nodes representing scalar variables
+	for elem in scalars:
+		# check if node defines attribute 'causality'
+		if 'causality' in elem.attrib:
+			# get value of attribute causality
+			causality = elem.attrib['causality'] 
+			# remove node if causility is neither 'input' nor 'output'
+			if not ( causality == 'input' or causality == 'output' ):
+				modelVariables.remove( elem );
+			else:
+				# save type of scalar variable
+				value = elem.findall( ".//*[@declaredType]" )
+				if len( value ) > 1:
+					print "WARNING: scalar variable has more than one type defined"
+				declaredTypes.add( value[0].attrib['declaredType'] )
+		else:
+			# attribute 'causality' not defined --> delete XML node
+			modelVariables.remove( elem );
+
+	# get XML node listing the type definitions
+	typeDefinitions = root.find( "TypeDefinitions" )
+	
+	# get list of all XML nodes representing types
+	types = typeDefinitions.findall( "./Type" )
+	
+	# iterate through all XML nodes representing types
+	for elem in types:
+		# remove node if the type it declares is not used by remaining scalar variables
+		if elem.attrib['name'] not in declaredTypes:
+			typeDefinitions.remove( elem )
+	
+	# return simpified XML model description as string
+	return et.tostring( root )
+
+
+
+def printInfo( zipfile ):
+
+        import datetime
+
+        for info in zipfile.infolist():
+                print info.filename
+                print '\tComment:\t', info.comment
+                print '\tModified:\t', datetime.datetime(*info.date_time)
+                print '\tSystem:\t\t', info.create_system, '(0 = Windows, 3 = Unix)'
+                print '\tZIP version:\t', info.create_version
+                print '\tCompressed:\t', info.compress_size, 'bytes'
+                print '\tUncompressed:\t', info.file_size, 'bytes'
+                print
+
+
+
+if __name__ == '__main__':
+
+	import sys, os, zipfile
+
+        import zlib
+
+	if len( sys.argv ) != 2:
+		print 'Usage:\n\tpython simplifyModelDescription.py <fmu-file-name>\n'
+		sys.exit()
+
+	fmuFileName = sys.argv[1]
+
+        # check if specified file is indeed a zip file
+	if not zipfile.is_zipfile( fmuFileName ):
+		print '%s is not a valid ZIP archive' % fmuFileName
+		sys.exit()
+
+        # access FMU
+	fmuIn = zipfile.ZipFile( fmuFileName, 'r' )
+
+        # output zip archive
+        tmpFmuFileName = fmuFileName + '.tmp'
+        fmuOut = zipfile.ZipFile( tmpFmuFileName, 'w', compression = zipfile.ZIP_DEFLATED )
+
+	try:
+                # access FMU model description                
+		xmlModelDescription = fmuIn.read( 'modelDescription.xml' )
+
+                # simplify model description
+		simplifiedModelDesciption = simplifyModelDescription( xmlModelDescription )
+
+                # iterate through content of FMU
+                for item in fmuIn.infolist():
+                        # write all content to new archive, but use the simlified model description
+                        if not item.filename == 'modelDescription.xml':
+                                buffer = fmuIn.read( item.filename )
+                                fmuOut.writestr( item, buffer )
+                        else:
+                                fmuOut.writestr( 'modelDescription.xml', simplifiedModelDesciption )
+	except KeyError:
+		print '\nInvalid FMU: no modelDescription.xml found in %s\n' % fmuFileName
+
+        # print info about simplified FMU
+        printInfo( fmuOut )
+
+        # close zip archives
+        fmuIn.close()
+        fmuOut.close()
+
+        # replace original FMU with simplified FMU
+        os.remove( fmuFileName );
+        os.rename( tmpFmuFileName, fmuFileName )
