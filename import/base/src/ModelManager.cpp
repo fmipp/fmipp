100d7b6ced3ff8162597653d2c3f81d18895f577
Edmund Widl <Edmund.Widl@ait.ac.at>
2016-01-13 17:26:41 +0100

    replaced nullptr with 0 (compatible with old MSVC versions)

--- import/base/src/ModelManager.cpp
+++ import/base/src/ModelManager.cpp
@@ -855,7 +855,7 @@ string ModelManager::getLastErrorAsString()
     if( 0 == errorMessageID )
         return string(); //No error message has been recorded
 
-    LPSTR messageBuffer = nullptr;
+    LPSTR messageBuffer = 0;
     size_t size = FormatMessageA( FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
 				  NULL, errorMessageID, MAKELANGID( LANG_NEUTRAL, SUBLANG_DEFAULT ), (LPSTR)&messageBuffer, 0, NULL );
 
