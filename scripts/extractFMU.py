003f208c935442c34179cb94a054ffc8d46d042f
Edmund Widl <Edmund.Widl@ait.ac.at>
2016-02-04 14:11:53 +0100

    added Python script for extracting FMUs, restructured script for simplifying FMUs

---
+++ scripts/extractFMU.py
@@ -0,0 +1,54 @@
+
+# Extract an FMU.
+def extractFMU( fmuFilePath, outputDirPath ):
+
+	import os, zipfile
+
+	# Check if specified file is indeed a zip file.
+	if not zipfile.is_zipfile( fmuFilePath ):
+		print '%s is not a valid ZIP archive' % fmuFilePath
+		return
+
+	# Check if output directory exists.
+	if not os.path.isdir( outputDirPath ):
+		print '%s is not a valid path' % outputDirPath
+		return
+
+	try:
+		# Extract FMU file name from complete path.
+		fmuFileName = os.path.split( fmuFilePath )[1];
+		# Split FMU file name (name & extension).
+		fmuSplitFileName = os.path.splitext( fmuFileName )
+
+		# Check if this is a valid FMU file name.
+		if '.fmu' != fmuSplitFileName[1]:
+			print '%s is not a valid FMU file name' % fmuFileName
+
+		# Extract model name from FMU file name.
+		fmuModelName = fmuSplitFileName[0]
+
+		# Access FMU.
+		fmu = zipfile.ZipFile( fmuFilePath, 'r' )
+
+		# Create sub-directory in output directory.
+		extractDirPath = os.path.join( outputDirPath, fmuModelName )
+                os.mkdir( extractDirPath )
+
+		# Extract FMU to output directory.
+		fmu.extractall( extractDirPath )
+        except:
+                print 'failed to extract file %s' % fmuFilePath
+
+
+if __name__ == '__main__':
+
+        import sys
+
+	if len( sys.argv ) != 3:
+		print 'Usage:\n\tpython extractFMU.py <path-to-fmu> <path-to-output-dir>\n'
+		sys.exit()
+
+	fmuFilePath = sys.argv[1]
+	outputDirPath = sys.argv[2]
+
+	extractFMU( fmuFilePath, outputDirPath )
