21f1cf9e9cbda02f2bbbda0ae3e2cb77a4cc6d0a
Edmund Widl <edmund.widl@ait.ac.at>
2016-10-03 14:56:50 +0200

    added new class 'BackEndImplementationBase' for easier creation of FMU cs; adapted test FMU 'sine_standalone' accordingly; test FMUs do not complain anymore in case no JAVA JAR utility is found;

---
+++ export/include/BackEndImplementationBase.h
@@ -0,0 +1,139 @@
+#ifndef _FMIPP_BACKENDIMPLEMENTATIONBASE_H
+#define _FMIPP_BACKENDIMPLEMENTATIONBASE_H
+
+#include <string>
+#include <vector>
+
+#include "export/include/FMIComponentBackEnd.h"
+
+
+class __FMI_DLL BackEndImplementationBase {
+	
+public:
+
+	/// Constructor.
+	BackEndImplementationBase();
+
+	/// Destructor.
+	virtual ~BackEndImplementationBase();
+
+	/// Initialization method.
+	int initializeBase( int argc, const char* argv[] );
+
+	/// Simulation method.
+	int doStepBase();
+
+	/// Enforce a specific time step length.
+	void enforceTimeStep( const fmiReal& fixedTimeStep );
+
+	/// Call the logger.
+	void logger( fmiStatus status, const std::string& category, const std::string& msg );
+
+protected:
+	
+	/** This function initializes the backend's scalar variables (parameters, inputs, outputs).
+	 *  Only calls to 'addRealInput(...)', 'addRealOutput(...)', etc. are allowed.
+	 *  To be implemented by the inheriting class.
+	 */
+	virtual void initializeScalarVariables() = 0;
+	
+	/** This function will be called after 'initializeScalarVariables'. 
+	 *  It is intended to initialize the back end (i.e., everything except the scalar variables).
+	 *  The function arguments are the command line arguments according to the model description.
+	 *  To be implemented by the inheriting class.
+	 */
+	virtual int initializeBackEnd( int argc, const char* argv[] ) = 0;
+
+	/** This function will be called whenever the front end's 'doStep(...)' methid is called.
+	 *  To be implemented by the inheriting class.
+	 */
+	virtual int doStep( const fmiReal& syncTime, const fmiReal& lastSyncTime ) = 0;
+
+protected:
+
+	std::vector<fmiReal*> realParams_;
+	std::vector<std::string> realParamNames_;
+
+	std::vector<fmiInteger*> integerParams_;
+	std::vector<std::string> integerParamNames_;
+
+	std::vector<fmiBoolean*> booleanParams_;
+	std::vector<std::string> booleanParamNames_;
+
+	std::vector<std::string*> stringParams_;
+	std::vector<std::string> stringParamNames_;
+	
+
+	std::vector<fmiReal*> realInputs_;
+	std::vector<std::string> realInputNames_;
+
+	std::vector<fmiInteger*> integerInputs_;
+	std::vector<std::string> integerInputNames_;
+
+	std::vector<fmiBoolean*> booleanInputs_;
+	std::vector<std::string> booleanInputNames_;
+
+	std::vector<std::string*> stringInputs_;
+	std::vector<std::string> stringInputNames_;
+
+
+	std::vector<fmiReal*> realOutputs_;
+	std::vector<std::string> realOutputNames_;
+
+	std::vector<fmiInteger*> integerOutputs_;
+	std::vector<std::string> integerOutputNames_;
+
+	std::vector<fmiBoolean*> booleanOutputs_;
+	std::vector<std::string> booleanOutputNames_;
+
+	std::vector<std::string*> stringOutputs_;
+	std::vector<std::string> stringOutputNames_;
+
+
+private:
+
+	FMIComponentBackEnd* backend_; ///< Internal pointer to backend.
+
+	fmiReal syncTime_;
+	fmiReal lastSyncTime_;
+
+	fmiStatus initParameters(); ///< Initialize paramters.
+	fmiStatus getParameters(); ///< Get paramter values.
+
+	fmiStatus initInputs(); ///< Initialize input variables.
+	fmiStatus getInputs(); ///< Get input variable values.
+
+	fmiStatus initOutputs(); ///< Initialize output variables.
+	fmiStatus setOutputs(); ///< Set output variable values.
+	
+	void writeScalarVariableNamesToFile(); ///< Write the names of all scalar variables to files.
+	void writeVectorContentToFile( const std::vector<std::string>& vec, const std::string& filename ) const; ///< Write the contents of a vector of strings to file.
+};
+
+
+#define addRealParameter( var ) { realParamNames_.push_back( #var ); realParams_.push_back( &var ); }
+#define addIntegerParameter( var ) { integerParamNames_.push_back( #var ); integerParams_.push_back( &var ); }
+#define addBooleanParameter( var ) { booleanParamNames_.push_back( #var ); booleanParams_.push_back( &var ); }
+#define addStringParameter( var ) { stringParamNames_.push_back( #var ); stringParams_.push_back( &var ); }
+
+
+#define addRealInput( var ) { realInputNames_.push_back( #var ); realInputs_.push_back( &var ); }
+#define addIntegerInput( var ) { integerInputNames_.push_back( #var ); integerInputs_.push_back( &var ); }
+#define addBooleanInput( var ) { booleanInputNames_.push_back( #var ); booleanInputs_.push_back( &var ); }
+#define addStringInput( var ) { stringInputNames_.push_back( #var ); stringInputs_.push_back( &var ); }
+
+
+#define addRealOutput( var ) { realOutputNames_.push_back( #var ); realOutputs_.push_back( &var ); }
+#define addIntegerOutput( var ) { integerOutputNames_.push_back( #var ); integerOutputs_.push_back( &var ); }
+#define addBooleanOutput( var ) { booleanOutputNames_.push_back( #var ); booleanOutputs_.push_back( &var ); }
+#define addStringOutput( var ) { stringOutputNames_.push_back( #var ); stringOutputs_.push_back( &var ); }
+
+
+#define CREATE_FMU_BACKEND( BACKENDTYPE ) \
+int main( int argc, const char* argv[] ) { \
+	BACKENDTYPE backend; \
+	if ( 0 != backend.initializeBase( argc, argv ) ) { return -1; } \
+	while ( true ) { if ( 0 != backend.doStepBase() ) return -1; } \
+	return 0; }
+
+#endif
---
+++ export/src/BackEndImplementationBase.cpp
@@ -0,0 +1,322 @@
+#include "export/include/BackEndImplementationBase.h"
+#include <fstream>
+#include <iostream>
+
+
+BackEndImplementationBase::BackEndImplementationBase() :
+	backend_( 0 )
+{}
+
+
+BackEndImplementationBase::~BackEndImplementationBase()
+{
+	if ( 0 != backend_ ) delete backend_;
+}
+
+
+int
+BackEndImplementationBase::initializeBase( int argc, const char* argv[] )
+{
+	// Special usage of the backend: Only write the names of the scalar
+	// variables (inputs, outputs, parameters) to files, then exit.
+	if ( ( 2 == argc ) && ( 0 == strcmp( argv[1], "--only-write-variable-names" ) ) )
+	{
+		initializeScalarVariables();
+		writeScalarVariableNamesToFile();
+		return 1;
+	}
+	
+	fmiStatus initParamsStatus = fmiOK;
+	fmiStatus initInputsStatus = fmiOK;
+	fmiStatus initOutputsStatus = fmiOK;
+
+	try
+	{
+		// Instantiate new backend.
+		backend_ = new FMIComponentBackEnd;
+		
+		// Init backend.
+		backend_->startInitialization();
+
+		// User defined initialization.
+		initializeScalarVariables();
+		initializeBackEnd( argc, argv );
+	
+		initParamsStatus = initParameters();
+		initInputsStatus = initInputs();
+		initOutputsStatus = initOutputs();
+
+		syncTime_ = backend_->getCurrentCommunicationPoint();
+		lastSyncTime_ = syncTime_;	
+
+		backend_->endInitialization();
+	}
+	catch (...) { return -1; }
+	
+	if ( ( initParamsStatus != fmiOK ) || ( initInputsStatus != fmiOK ) || ( initOutputsStatus != fmiOK ) )
+		return -1;
+	
+	return 0;
+}
+
+
+int
+BackEndImplementationBase::doStepBase()
+{
+	static fmiStatus getParamsStatus = fmiOK;
+	static fmiStatus getInputsStatus = fmiOK;
+	static fmiStatus setOutputsStatus = fmiOK;
+	static int stepStatus = 0;
+
+	try
+	{
+		backend_->waitForMaster();
+
+		syncTime_ = backend_->getCurrentCommunicationPoint() + backend_->getCommunicationStepSize();
+
+		getParamsStatus = getParameters();
+		getInputsStatus = getInputs();
+
+		if ( 0 != ( stepStatus = doStep( syncTime_, lastSyncTime_ ) ) ) {
+			logger( fmiError, "ERROR", "doStep failed" );
+		}
+
+		setOutputsStatus = setOutputs();
+	
+		lastSyncTime_ = syncTime_;	
+
+		backend_->signalToMaster();
+	}
+	catch (...) { return -1; }
+
+	if ( ( getParamsStatus != fmiOK ) || ( getInputsStatus != fmiOK ) || ( setOutputsStatus != fmiOK ) || ( stepStatus != 0 ) )
+		return -1;
+	
+	return 0;
+}
+
+
+void
+BackEndImplementationBase::enforceTimeStep( const fmiReal& fixedTimeStep )
+{
+	backend_->enforceTimeStep( fixedTimeStep );
+}
+
+
+void
+BackEndImplementationBase::logger( fmiStatus status, const std::string& category, const std::string& msg )
+{
+	backend_->logger( status, category, msg );
+}
+
+
+fmiStatus
+BackEndImplementationBase::initParameters()
+{
+	fmiStatus init;
+	
+	if ( fmiOK != ( init = backend_->initializeRealParameters( realParamNames_ ) ) ) {
+		logger( fmiError, "ERROR", "initializeRealParameters failed" );
+		return init;
+	}
+	
+	if ( fmiOK != ( init = backend_->initializeIntegerParameters( integerParamNames_ ) ) ) {
+		logger( fmiError, "ERROR", "initializeIntegerParameters failed" );
+		return init;
+	}
+	
+	if ( fmiOK != ( init = backend_->initializeBooleanParameters( booleanParamNames_ ) ) ) {
+		logger( fmiError, "ERROR", "initializeBooleanParameters failed" );
+		return init;
+	}
+	
+	if ( fmiOK != ( init = backend_->initializeStringParameters( stringParamNames_ ) ) ) {
+		logger( fmiError, "ERROR", "initializeStringParameters failed" );
+		return init;
+	}
+	
+	return fmiOK;
+}
+
+fmiStatus
+BackEndImplementationBase::getParameters()
+{
+	static fmiStatus status;
+	
+	if ( fmiOK != ( status = backend_->getRealParameters( realParams_ ) ) ) {
+		logger( fmiError, "ERROR", "getRealParameters failed" );
+		return status;
+	}
+
+	if ( fmiOK != ( status = backend_->getIntegerParameters( integerParams_ ) ) ) {
+		logger( fmiError, "ERROR", "getIntegerParameters failed" );
+		return status;
+	}
+
+	if ( fmiOK != ( status = backend_->getBooleanParameters( booleanParams_ ) ) ) {
+		logger( fmiError, "ERROR", "getBooleanParameters failed" );
+		return status;
+	}
+
+	if ( fmiOK != ( status = backend_->getStringParameters( stringParams_ ) ) ) {
+		logger( fmiError, "ERROR", "getStringParameters failed" );
+		return status;
+	}
+
+	return fmiOK;
+}
+
+
+fmiStatus
+BackEndImplementationBase::initInputs()
+{
+	fmiStatus init;
+
+	if ( fmiOK != ( init = backend_->initializeRealInputs( realInputNames_ ) ) ) {
+		logger( fmiError, "ERROR", "initializeRealInputs failed" );
+		return init;
+	}
+	
+	if ( fmiOK != ( init = backend_->initializeIntegerInputs( integerInputNames_ ) ) ) {
+		logger( fmiError, "ERROR", "initializeIntegerInputs failed" );
+		return init;
+	}
+	
+	if ( fmiOK != ( init = backend_->initializeBooleanInputs( booleanInputNames_ ) ) ) {
+		logger( fmiError, "ERROR", "initializeBooleanInputs failed" );
+		return init;
+	}
+	
+	if ( fmiOK != ( init = backend_->initializeStringInputs( stringInputNames_ ) ) ) {
+		logger( fmiError, "ERROR", "initializeStringInputs failed" );
+		return init;
+	}
+	
+	return fmiOK;
+}
+
+
+fmiStatus
+BackEndImplementationBase::getInputs()
+{
+	static fmiStatus status;
+	
+	if ( fmiOK != ( status = backend_->getRealInputs( realInputs_ ) ) ) {
+		logger( fmiError, "ERROR", "getRealInputs failed" );
+		return status;
+	}
+
+	if ( fmiOK != ( status = backend_->getIntegerInputs( integerInputs_ ) ) ) {
+		logger( fmiError, "ERROR", "getIntegerInputs failed" );
+		return status;
+	}
+
+	if ( fmiOK != ( status = backend_->getBooleanInputs( booleanInputs_ ) ) ) {
+		logger( fmiError, "ERROR", "getBooleanInputs failed" );
+		return status;
+	}
+
+	if ( fmiOK != ( status = backend_->getStringInputs( stringInputs_ ) ) ) {
+		logger( fmiError, "ERROR", "getStringInputs failed" );
+		return status;
+	}
+
+	return fmiOK;
+}
+
+
+fmiStatus
+BackEndImplementationBase::initOutputs()
+{
+	fmiStatus init;
+	
+	if ( fmiOK != ( init = backend_->initializeRealOutputs( realOutputNames_ ) ) ) {
+		logger( fmiError, "ERROR", "initializeRealOutputs failed" );
+		return init;
+	}
+	
+	if ( fmiOK != ( init = backend_->initializeIntegerOutputs( integerOutputNames_ ) ) ) {
+		logger( fmiError, "ERROR", "initializeIntegerOutputs failed" );
+		return init;
+	}
+	
+	if ( fmiOK != ( init = backend_->initializeBooleanOutputs( booleanOutputNames_ ) ) ) {
+		logger( fmiError, "ERROR", "initializeBooleanOutputs failed" );
+	}
+	
+	if ( fmiOK != ( init = backend_->initializeStringOutputs( stringOutputNames_ ) ) ) {
+		logger( fmiError, "ERROR", "initializeStringOutputs failed" );
+		return init;
+	}
+
+	return fmiOK;
+}
+
+
+fmiStatus
+BackEndImplementationBase::setOutputs()
+{
+	static fmiStatus status;
+	
+	if ( fmiOK != ( status = backend_->setRealOutputs( realOutputs_ ) ) ) {
+		logger( fmiError, "ERROR", "setRealOutputs failed" );
+		return status;
+	}
+
+	if ( fmiOK != ( status = backend_->setIntegerOutputs( integerOutputs_ ) ) ) {
+		logger( fmiError, "ERROR", "setIntegerOutputs failed" );
+		return status;
+	}
+
+	if ( fmiOK != ( status = backend_->setBooleanOutputs( booleanOutputs_ ) ) ) {
+		logger( fmiError, "ERROR", "setBooleanOutputs failed" );
+		return status;
+	}
+
+	if ( fmiOK != ( status = backend_->setStringOutputs( stringOutputs_ ) ) ) {
+		logger( fmiError, "ERROR", "setStringOutputs failed" );
+		return status;
+	}
+
+	return fmiOK;
+}
+
+
+void
+BackEndImplementationBase::writeScalarVariableNamesToFile()
+{
+	// Write parameter names.
+	writeVectorContentToFile( realParamNames_, "real.param" );
+	writeVectorContentToFile( integerParamNames_, "integer.param" );
+	writeVectorContentToFile( booleanParamNames_, "boolean.param" );
+	writeVectorContentToFile( stringParamNames_, "string.param" );
+
+	// Write input names.
+	writeVectorContentToFile( realInputNames_, "real.in" );
+	writeVectorContentToFile( integerInputNames_, "integer.in" );
+	writeVectorContentToFile( booleanInputNames_, "boolean.in" );
+	writeVectorContentToFile( stringInputNames_, "string.in" );
+
+	// Write output names.
+	writeVectorContentToFile( realOutputNames_, "real.out" );
+	writeVectorContentToFile( integerOutputNames_, "integer.out" );
+	writeVectorContentToFile( booleanOutputNames_, "boolean.out" );
+	writeVectorContentToFile( stringOutputNames_, "string.out" );
+}
+
+
+/// Write the contents of a vector of strings to file.
+void
+BackEndImplementationBase::writeVectorContentToFile( const std::vector<std::string>& vec, const std::string& filename ) const
+{
+	// Check if vector is empty.
+	if ( true == vec.empty() ) return;
+	
+	std::ofstream out( filename ); // New output file.
+	
+	std::vector<std::string>::const_iterator begin = vec.begin();
+	std::vector<std::string>::const_iterator end = vec.end();
+	std::vector<std::string>::const_iterator it;
+	// Iterate through vector of strings and write each element into a separate line.
+	for ( it = begin; it != end; ++it ) out << (*it) << std::endl;
+}
