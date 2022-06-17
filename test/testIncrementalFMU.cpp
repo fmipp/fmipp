b27dbea1bee14af2c6e8ded851f03a027f602cb8
Edmund Widl <edmund.widl@ait.ac.at>
2022-06-17 09:27:16 +0200

    replace deprecated include statement

--- test/testIncrementalFMU.cpp
+++ test/testIncrementalFMU.cpp
@@ -12,7 +12,7 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE testIncrementalFMU
 #include <boost/test/unit_test.hpp>
-#include <boost/test/floating_point_comparison.hpp>
+#include <boost/test/tools/floating_point_comparison.hpp>
 #include <cmath>
 #include <iostream>
 #include <algorithm>
